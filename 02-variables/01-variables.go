04e081777ad49155a4c98eae33f109d14b281a47
Magesh Kuppan <tkmagesh77@gmail.com>
2023-01-09 12:05:16 +0530

    Using multiple variables

--- 02-variables/01-variables.go
+++ 02-variables/01-variables.go
@@ -38,4 +38,93 @@ func main() {
 		fmt.Println(i) // reading the value of "i" and there by "using" it
 	*/
 
+	/* More than 1 varibale */
+	/*
+		var x int
+		var y int
+		var result int
+		var str string
+		x = 100
+		y = 200
+		result = x + y
+		str = "Sum of %d and %d is %d\n"
+		fmt.Printf(str, x, y, result)
+	*/
+
+	/*
+		var x, y, result int
+		var str string
+		x = 100
+		y = 200
+		result = x + y
+		str = "Sum of %d and %d is %d\n"
+		fmt.Printf(str, x, y, result)
+	*/
+
+	/*
+		var (
+			x, y, result int
+			str          string
+		)
+		x = 100
+		y = 200
+		result = x + y
+		str = "Sum of %d and %d is %d\n"
+		fmt.Printf(str, x, y, result)
+	*/
+
+	//Declaration & initialization
+	/*
+		var x int = 100
+		var y int = 200
+		var result int = x + y
+		var str string = "Sum of %d and %d is %d\n"
+		fmt.Printf(str, x, y, result)
+	*/
+
+	/*
+		var x, y int = 100, 200
+		var result int = x + y
+		var str string = "Sum of %d and %d is %d\n"
+		fmt.Printf(str, x, y, result)
+	*/
+
+	/*
+		var (
+			x, y   int    = 100, 200
+			result int    = x + y
+			str    string = "Sum of %d and %d is %d\n"
+		)
+		fmt.Printf(str, x, y, result)
+	*/
+
+	//type inference
+	/*
+		var x, y = 100, 200
+		var result = x + y
+		var str = "Sum of %d and %d is %d\n"
+		fmt.Printf(str, x, y, result)
+	*/
+
+	/*
+		var (
+			x, y   = 100, 200
+			result = x + y
+			str    = "Sum of %d and %d is %d\n"
+		)
+		fmt.Printf(str, x, y, result)
+	*/
+
+	/*
+		var (
+			x, y, str = 100, 200, "Sum of %d and %d is %d\n"
+			result    = x + y
+		)
+		fmt.Printf(str, x, y, result)
+	*/
+
+	x, y, str := 100, 200, "Sum of %d and %d is %d\n"
+	result := x + y
+	fmt.Printf(str, x, y, result)
+
 }
