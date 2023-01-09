a6b7ba9db5af0f953a90eab20edbccfd420e14b6
Magesh Kuppan <tkmagesh77@gmail.com>
2023-01-09 14:50:09 +0530

    assignment-03 problem statement

---
+++ 04-functions/assignment-03.go
@@ -0,0 +1,2 @@
+/* Refactor the solution for assignment-02 using functions */
+/* Make sure that each function has ONLY ONE responsibility (in other words, there should be ONLY ONE reason for a function to change) */
