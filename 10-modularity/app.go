a264a01edad8ceba23303629bfd9a44c356db942
Magesh Kuppan <tkmagesh77@gmail.com>
2023-01-10 12:31:05 +0530

    modules & packages - modules introduction

---
+++ 10-modularity/app.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("app invoked")
+	fmt.Println(greet("Magesh"))
+}
---
+++ 10-modularity/go.mod
@@ -0,0 +1,3 @@
+module github.com/tkmagesh/cisco-go-jan-2023/10-modularity
+
+go 1.18
