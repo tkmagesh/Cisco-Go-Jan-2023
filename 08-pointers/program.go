34413c648c7d07148d070dec19fd1250ae95ef79
Magesh Kuppan <tkmagesh77@gmail.com>
2023-01-10 09:54:34 +0530

    pointer problems

--- 08-pointers/program.go
+++ 08-pointers/program.go
@@ -19,4 +19,22 @@ func main() {
 	fmt.Println(v)
 
 	fmt.Println(no == *(&no))
+
+	x := 100
+	fmt.Println("Before incrementing, x = ", x)
+	increment(x)
+	fmt.Println("After incrementing, x = ", x)
+
+	n1, n2 := 100, 200
+	fmt.Printf("Before swapping, n1 = %d and n2 = %d\n", n1, n2)
+	swap(n1, n2)
+	fmt.Printf("After swapping, n1 = %d and n2 = %d\n", n1, n2)
+}
+
+func increment(no int) /* do not return */ {
+	no++
+}
+
+func swap(n1, n2 int) /* do not return */ {
+	/*  */
 }
