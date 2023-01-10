823588f5dfa158be9e75be72ed16d2af299f1ed0
Magesh Kuppan <tkmagesh77@gmail.com>
2023-01-10 16:23:00 +0530

    assignment-06 solution

--- 11-structs/assignment-06.go
+++ 11-structs/assignment-06.go
@@ -9,3 +9,34 @@ Write the following functions
 	FormatProduct -> return a formatted string with all the field values of the given product
 	ApplyDiscount -> applies the given discount in percentage (update the cost) on the given product
 */
+
+package main
+
+import "fmt"
+
+type Product struct {
+	Id       int
+	Name     string
+	Cost     float32
+	Category string
+}
+
+func main() {
+	pen := Product{
+		Id:       100,
+		Name:     "Pen",
+		Cost:     10,
+		Category: "Stationary",
+	}
+	fmt.Println("Before Applying discount :", FormatProduct(pen))
+	ApplyDiscount(&pen, 10)
+	fmt.Println("After Applying discount :", FormatProduct(pen))
+}
+
+func FormatProduct(product Product) string {
+	return fmt.Sprintf("Id = %d, Name = %q, Cost = %.2f, Category = %q", product.Id, product.Name, product.Cost, product.Category)
+}
+
+func ApplyDiscount(product *Product, discount float32) {
+	product.Cost = product.Cost * ((100 - discount) / 100)
+}
