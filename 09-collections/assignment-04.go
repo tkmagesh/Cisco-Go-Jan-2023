29ab8605878d087765575ea9d0e07a651608e7ba
Magesh Kuppan <tkmagesh77@gmail.com>
2023-01-10 12:08:10 +0530

    assignments 04 & 05 - solutions

--- 09-collections/assignment-04.go
+++ 09-collections/assignment-04.go
@@ -2,3 +2,31 @@
 write a function "genPrimes" that generates the prime numbers from the given start to end and returns the primeNos
 the main function receives the prime nos and prints them
 */
+
+package main
+
+import "fmt"
+
+func main() {
+	primeNos := genPrimes(3, 100)
+	fmt.Println(primeNos)
+}
+
+func genPrimes(start, end int) []int {
+	primeNos := make([]int, 0)
+	for no := start; no <= end; no++ {
+		if isPrime(no) {
+			primeNos = append(primeNos, no)
+		}
+	}
+	return primeNos
+}
+
+func isPrime(no int) bool {
+	for i := 2; i <= (no / 2); i++ {
+		if no%i == 0 {
+			return false
+		}
+	}
+	return true
+}
