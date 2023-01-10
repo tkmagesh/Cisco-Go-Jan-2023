8630c5ddb7c40f8cf98e6b2da84589ab25b5fdf9
Magesh Kuppan <tkmagesh77@gmail.com>
2023-01-10 15:54:46 +0530

    assignment-06 problem statement

---
+++ 11-structs/assignment-06.go
@@ -0,0 +1,11 @@
+/*
+Create a "Product" struct with the following fields
+	Id
+	Name
+	Cost
+	Category
+
+Write the following functions
+	FormatProduct -> return a formatted string with all the field values of the given product
+	ApplyDiscount -> applies the given discount in percentage (update the cost) on the given product
+*/
